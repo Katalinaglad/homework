ec473a291cda9aab0fb48541e67aa2e90c2cca0d
Katalinaglad <katagladkova762@gmail.com>
2024-10-24 15:47:23 +0300

    Add files via upload

---
+++ domashka1.php
@@ -0,0 +1,84 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Калькулятор дней</title>
+</head>
+<body>
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+        <label for="startDateDay">День начала:</label>
+        <input type="number" name="startDateDay" id="startDateDay" min="1" max="31" required><br>
+        
+        <label for="startDateMonth">Месяц начала:</label>
+        <select name="startDateMonth" id="startDateMonth">
+            <option value="1" selected>Январь</option>
+            <option value="2">Февраль</option>
+            <option value="3">Март</option>
+            <option value="4">Апрель</option>
+            <option value="5">Май</option>
+            <option value="6">Июнь</option>
+            <option value="7">Июль</option>
+            <option value="8">Август</option>
+            <option value="9">Сентябрь</option>
+            <option value="10">Октябрь</option>
+            <option value="11">Ноябрь</option>
+            <option value="12">Декабрь</option>
+        </select><br>
+        
+        <label for="startDateYear">Год начала:</label>
+        <input type="number" name="startDateYear" id="startDateYear" min="1970" max="2040" required><br>
+        
+        <label for="endDateDay">День окончания:</label>
+        <input type="number" name="endDateDay" id="endDateDay" min="1" max="31" required><br>
+        
+        <label for="endDateMonth">Месяц окончания:</label>
+        <select name="endDateMonth" id="endDateMonth">
+            <option value="1" selected>Январь</option>
+            <option value="2">Февраль</option>
+            <option value="3">Март</option>
+            <option value="4">Апрель</option>
+            <option value="5">Май</option>
+            <option value="6">Июнь</option>
+            <option value="7">Июль</option>
+            <option value="8">Август</option>
+            <option value="9">Сентябрь</option>
+            <option value="10">Октябрь</option>
+            <option value="11">Ноябрь</option>
+            <option value="12">Декабрь</option>
+        </select><br>
+        
+        <label for="endDateYear">Год окончания:</label>
+        <input type="number" name="endDateYear" id="endDateYear" min="1970" max="2040" required><br>
+        <button type="submit">Посчитать</button>
+    </form>
+    
+    <?php
+    if (isset($_POST['startDateDay']) && isset($_POST['startDateMonth']) && isset($_POST['startDateYear'])) {
+        $startDate = date("Y-m-d", strtotime("{$_POST['startDateDay']}-{$_POST['startDateMonth']}-{$_POST['startDateYear']}"));
+
+        if (isset($_POST['endDateDay']) && isset($_POST['endDateMonth']) && isset($_POST['endDateYear'])) {
+            $endDate = date("Y-m-d", strtotime("{$_POST['endDateDay']}-{$_POST['endDateMonth']}-{$_POST['endDateYear']}"));
+            $timeDifference = abs(strtotime($endDate) - strtotime($startDate));
+            $days = floor($timeDifference / (60 * 60 * 24));
+            $totalMinutes = floor($timeDifference / 60);
+            
+            echo "<h2>Разница между датами:</h2>";
+            echo "Количество дней: " . $days . "<br>";
+            echo "Общее количество минут: " . $totalMinutes . "<br>";
+        } else {
+            echo "<h2>Неправильно введены конечные даты.</h2>";
+        }
+    }
+    ?>
+    
+    <?php
+    if (isset($_POST['startDateDay']) && isset($_POST['startDateMonth']) && isset($_POST['startDateYear'])) {
+        session_start();
+        $_SESSION['startDate'] = array('day' => $_POST['startDateDay'], 'month' => $_POST['startDateMonth'], 'year' => $_POST['startDateYear']);
+    }
+    ?>
+</body>
+</html>
---
+++ domashka2.php
@@ -0,0 +1,59 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $name = trim($_POST['name']);
+    $email = trim($_POST['email']);
+    $messageText = trim($_POST['message']);
+    
+    if (!empty($name) && !empty($email) && !empty($messageText)) {
+        $dateTime = new DateTime('now', new DateTimeZone('Europe/Moscow'));
+        $name = htmlspecialchars($name, ENT_QUOTES);
+        $email = htmlspecialchars($email, ENT_QUOTES);
+        $messageText = htmlspecialchars($messageText, ENT_QUOTES);
+        $messageText = nl2br($messageText); 
+        $currentDateTime = $dateTime->format('Y-m-d H:i:s');
+        $newEntry = $name . '|' . $email . '|' . $currentDateTime . '|' . $messageText . PHP_EOL;
+        file_put_contents('data.txt', $newEntry, FILE_APPEND);
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Гостевая книга</title>
+</head>
+<body>
+    <h1>Оставьте ваше сообщение</h1>
+    <form method="post" action="">
+        <label for="name">Имя:</label><br>
+        <input type="text" id="name" name="name" required><br><br>
+        
+        <label for="email">Электронная почта:</label><br>
+        <input type="email" id="email" name="email" required><br><br>
+        
+        <label for="message">Текст сообщения:</label><br>
+        <textarea id="message" name="message" required></textarea><br><br>
+        
+        <button type="submit">Отправить</button>
+    </form>
+
+    <h2>Сообщения гостей</h2>
+
+    <?php
+    if (file_exists('data.txt')) {
+        $allMessages = file('data.txt'); 
+        $allMessages = array_reverse($allMessages);     
+        foreach ($allMessages as $singleMessage) {
+            list($name, $email, $dateTime, $text) = explode('|', $singleMessage);
+            echo "<div>";
+            echo "<strong>{$name}</strong> <br> {$email} <br> {$dateTime} <br>";
+            echo "<p>- {$text}</p>";
+            echo "</div><hr>";
+        }
+    } else {
+        echo "<p>Пока нет сообщений.</p>";
+    }
+    ?>
+</body>
+</html>
